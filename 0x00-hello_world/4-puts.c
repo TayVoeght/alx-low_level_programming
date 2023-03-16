efc041ad77fbff521e86fbfd6d56249e666acab4
UserName <email@gmail..com>
2023-03-16 13:07:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#include <stadio.h>
-
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+	return (0)
 }
